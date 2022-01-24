812b84430eaeb223d5b6f1e725a72ed5a3dc4bd4
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-24 19:46:22 +0800

    added the view to register

--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import UserLoginForm
-from django.contrib.auth import authenticate, login, logout
+from .forms import UserLoginForm, UserRegisterForm
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
 
 from django.urls import reverse_lazy
 
+User = get_user_model()
 # Create your views here.
 def login_view(request):
 	form = UserLoginForm(request.POST or None)
@@ -20,6 +21,26 @@ def login_view(request):
 		'form':form
 	}
 	return render(request, 'account/login.html', context)
+
+def register_view(request):
+	form = UserRegisterForm(request.POST or None)
+	if form.is_valid():
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+
+		# create a user 
+		user = User(username=username)
+		# user the builtin function to save the password properly
+		user.set_password(password)
+		# save the user to the database
+		user.save()
+
+		return redirect('login')
+	context = {
+		'form':form
+	}
+	return render(request, 'account/register.html', context)
+
 
 @login_required
 def logout_view(request):
