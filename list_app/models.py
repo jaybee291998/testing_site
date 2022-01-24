b30c2f9133da0f21a6dd08494831418550845e72
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-24 20:17:48 +0800

    created the model for the list

--- list_app/models.py
+++ list_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+class List(models.Model):
+	user 			= models.ForeignKey(User, related_name='my_list', on_delete=models.CASCADE)
+	name 			= models.CharField(max_length=64)
+	content			= models.TextField()
+	timestamp 		= models.DateTimeField(auto_now_add=True)
+	last_modified	= models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
