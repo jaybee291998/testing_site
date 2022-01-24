3b48d4aca1a76ac72735503ae398c9512501588b
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-24 19:08:09 +0800

    added login, logout and home view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserLoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from django.urls import reverse_lazy
 
 # Create your views here.
+def login_view(request):
+	form = UserLoginForm(request.POST or None)
+	if form.is_valid():
+		username = form.cleaned_data.get('username')
+		password = form.cleaned_data.get('password')
+
+		user = authenticate(username=username, password=password)
+		login(request, user)
+		return redirect('home')
+
+	context = {
+		'form':form
+	}
+	return render(request, 'account/login.html', context)
+
+@login_required
+def logout(request):
+	logout(request)
+	return redirect('login')
+
+@login_required
+def home_view(request):
+	context = {
+		'logout_link': reverse_lazy('logout'),
+		'message': 'pussy'
+	}
+	return render(request, 'account/home.html', context)
