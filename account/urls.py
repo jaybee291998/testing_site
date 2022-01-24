2a02c660206b7f5533733a6738dba00f04436295
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-24 19:48:32 +0800

    added the url for register

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path('login/', views.login_view, name='login'),
 	path('home/', views.home_view, name='home'),
-	path('logout/', views.logout_view, name='logout')
+	path('logout/', views.logout_view, name='logout'),
+	path('register/', views.register_view, name='register'),
 ]
