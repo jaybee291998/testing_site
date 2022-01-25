2506a16d581dea597b48416f3f6d135380322e9a
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-25 08:46:53 +0800

    added the API view for the basic CRUD operation

--- list_app/views.py
+++ list_app/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
+from django.http import Http404, HttpResponse, JsonResponse
+from django.urls import reverse_lazy
+
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+from rest_framework import status, generics
+
+
+from .models import List
+from .serializer import ListSerializer
 # Create your views here.
+@method_decorator(login_required, name='dispatch')
+class ListList(generics.ListCreateAPIView):
+	serializer_class = ListSerializer
+	# override get_queryset so that only the users list will be retrieved
+	def get_queryset(self):
+		return self.request.user.my_list.all()
+
+	# override the perform_create so that the user is saved
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+
+@method_decorator(login_required, name='dispatch')
+class ListDetail(generics.RetrieveUpdateDestroyAPIView):
+	serializer_class = ListSerializer
+	# override get_queryset so that only the users list will be retrieved
+	def get_queryset(self):
+		return self.request.user.my_list.all()	
