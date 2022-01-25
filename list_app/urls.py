cb8283b15ab3a769bab07c52b07164a88b721024
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-25 08:51:17 +0800

    added the path to the  ListList and ListDetail api views

--- list_app/urls.py
+++ list_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
-	
+	path('list-api/', views.ListList.as_view(), name='listlist-api'),
+	path('detail-api/', views.ListDetail.as_view(), name='listdetail-api'),
 ]
