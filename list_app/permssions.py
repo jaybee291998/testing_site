39a49fcd69d18baa5208a4329e2b48b846e4774e
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-25 20:48:44 +0800

    Create permssions.py

---
+++ list_app/permssions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+	"""
+		Only allow the owner of an object to retrieve, update or destroy the object
+	"""
+
+	def has_object_permission(self, request, view, obj):
+		return obj.user == self.request.user
