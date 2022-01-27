780232525ae53dde17eac8ea655795f1bf2211f3
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-01-27 18:01:35 +0800

    added checks to the createTable, so that it catches common mistakes using the function

--- static/js/ui.js
+++ static/js/ui.js
@@ -12,6 +12,11 @@ function showDivBase(div_to_show, divList){
 // table_class -> the html class of the table
 // titles -> sn array that contains the titles of the table, must have the same size as properties_to_show
 function createTable(raw_data, properties_to_show, row_selection_func, table_class, titles){
+	if(raw_data.length==0) throw 'raw_data is empty';
+	if(properties_to_show.length != titles.length) throw 'properties_to_show and titles has mismatching length' ;
+	properties_to_show.forEach((property)=>{
+		if(!raw_data.hasOwnProperty(property)) throw `${property} is not a property of the raw_data`;
+	})
 	let table = document.createElement('TABLE');
 	table.className = table_class;
 
