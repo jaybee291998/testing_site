c859471b9fc521967627fca2f87eaf1c32a8e1b0
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-02-01 00:31:19 +0800

    Update settings.py

--- testing_site/settings.py
+++ testing_site/settings.py
@@ -147,12 +147,14 @@ SESSION_COOKIE_HTTPONLY = True
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:8080',
+    'https://heuristic-jang-ed96a5.netlify.app/'
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:8080',
+    'https://heuristic-jang-ed96a5.netlify.app/'
 ]
 CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
 CORS_ALLOW_CREDENTIALS = True
