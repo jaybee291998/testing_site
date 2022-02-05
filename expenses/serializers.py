c89acc31119863fac808dfa4f476fca7215f5a26
jaybee291998 <91439919+jaybee291998@users.noreply.github.com>
2022-02-05 18:23:33 +0800

    override create
    so that every time an expense is created the price will be subtracted to the fund chosen

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -7,6 +7,29 @@ class ExpenseSerializer(serializers.ModelSerializer):
 		model = Expense
 		fields = ['id', 'description', 'category', 'price', 'timestamp', 'fund', 'account']
 
+	def validate_price(self, value):
+		"""
+			check if the price provided is appropriate
+		"""
+		if value <= 0:
+			raise serializers.ValidationError('Price cant be less than or equal to zero.')
+		return value
+
+	def validate(self, data):
+		"""
+			checks if the fund to be used has sufficient balance
+		"""
+		if data['price'] > data['fund'].amount:
+			raise serializers.ValidationError('Fund {fund.name} has an insufficient balance.\nCurrent balance: {fund.amout}')
+		return data;
+
+	def create(self, validated_data):
+		price = validated_data.get('price')
+		fund = validated_data.get('fund')
+		fund.withdraw(price)
+		fund.save()
+		return Expense.objects.create(**validated_data)
+
 class ExpenseTypeSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ExpenseType
